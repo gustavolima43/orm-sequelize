b16a15d126c988b9ab9400038ae829abe54f22af
Gustavo <gustavolimamendes2@gmail.com>
2023-05-28 14:17:11 -0300

    Referenciando tabelas

--- api/models/turma.js
+++ api/models/turma.js
@@ -3,29 +3,29 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Pessoas extends Model {
+  class Turma extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Pessoas.hasMany(models.Turmas, {
+      Turma.hasMany(models.Matriculas, {
+        foreignKey: 'turma_id'
+      })
+      Turma.belongsTo(models.Pesoas, {
         foreignKey: 'docente_idS'
-      })
-      Pessoas.hasMany(models.Matriculas, {
-        foreignKey: 'estudante_id'
-      })
+      });
+      Turma.belongsTo(models.Niveis, {
+        foreignKey: 'nivel_id'
+      });
     }
   }
-  Pessoas.init({
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
+  Turma.init({
+    data_inicio: DataTypes.DATEONLY
   }, {
     sequelize,
-    modelName: 'Pessoas',
+    modelName: 'Turma',
   });
-  return Pessoas;
+  return Turma;
 };
